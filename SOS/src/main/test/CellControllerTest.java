fb318ad1f92387865de08c1d4e2269198fb72999
noahturner02 <noahturner02@gmail.com>
2022-10-22 13:17:36 -0500

    A bunch of maven junk
    Added a billion dependencies and switched things around. basically integrated Junit 5

---
+++ SOS/src/main/test/CellControllerTest.java
@@ -0,0 +1,7 @@
+import com.example.sos.CellController;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CellControllerTest {
+    CellController c = new CellController();
+    
+}
