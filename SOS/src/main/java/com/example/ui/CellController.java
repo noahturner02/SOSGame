d4b848b22d45e9566abd8d97eab59ca2005e0122
noahturner02 <noahturner02@gmail.com>
2022-11-20 02:16:33 -0600

    Refactor clickEventHandler
    Separate into two separate functions. One for UI and one for data

--- SOS/src/main/java/com/example/ui/CellController.java
+++ SOS/src/main/java/com/example/ui/CellController.java
@@ -62,6 +62,103 @@ public class CellController {
 
     static Game game = new SimpleGame(3, PlayerType.HUMAN, PlayerType.HUMAN);
 
+    private void onClickUI(List<Coordinate> SOSList, StackPane cell) {
+        ObservableList<Node> childrenList = cell.getChildren();
+        if (SOSList.size() >= 3) {
+            while (SOSList.size() >= 3) {
+                StackPane startCell = getChildFromGridPaneByRowAndColumn(SOSList.get(0).getX(), SOSList.get(0).getY());
+                StackPane endCell = getChildFromGridPaneByRowAndColumn(SOSList.get(2).getX(), SOSList.get(2).getY());
+                double startX = startCell.getLayoutX() + (startCell.getWidth() / 2);
+                double startY = startCell.getLayoutY() + (startCell.getHeight() / 2);
+                double endX = endCell.getLayoutX() + (startCell.getWidth() / 2);
+                double endY = endCell.getLayoutY() + (startCell.getHeight() / 2);
+                Line line = new Line(startX, startY, endX, endY);
+                line.setStrokeWidth(5);
+                if (game.getPlayerTurn() == PlayerTurn.PLAYER2) {
+                    line.setStyle("-fx-stroke: red");
+                }
+                else if (game.getPlayerTurn() == PlayerTurn.PLAYER1) {
+                    line.setStyle("-fx-stroke: blue");
+                }
+                gridpaneWrapper.getChildren().add(line);
+                SOSList.remove(0);
+                SOSList.remove(0);
+                SOSList.remove(0);
+                System.out.println("Line created: " + startX + " " + startY + " " + endX + " " + endY);
+            }
+        }
+
+        switch (game.board.getCellByIndex(GridPane.getRowIndex(cell), GridPane.getColumnIndex(cell)).getStatus()) {
+            case S:
+                childrenList.get(0).setVisible(true);
+                break;
+            case O:
+                childrenList.get(1).setVisible(true);
+                break;
+        }
+        if (game.getPlayerTurn() == PlayerTurn.PLAYER1) {
+            player2Pane.setStyle("-fx-background-color: #FFFFFF");
+            player1Pane.setStyle("-fx-background-color: #6D9DD5");
+        } else if (game.getPlayerTurn() == PlayerTurn.PLAYER2) {
+            player2Pane.setStyle("-fx-background-color: #CC4B23");
+            player1Pane.setStyle("-fx-background-color: #FFFFFF");
+        }
+    }
+
+    private void onClickData(StackPane cell) {
+        // Processing click in game logic
+        List<Coordinate> SOSList = new ArrayList<>();
+        Coordinate c;
+        if (game.board.getCellByIndex(GridPane.getRowIndex(cell), GridPane.getColumnIndex(cell)).getStatus() == cellStatus.EMPTY) {
+            if (game.getPlayerTurn() == PlayerTurn.PLAYER1) {
+                if (game.getPlayer1PieceSelected() == SelectedPiece.S) {
+                    game.board.getCellByIndex(GridPane.getRowIndex(cell), GridPane.getColumnIndex(cell)).setStatus(cellStatus.S);
+                } else if (game.getPlayer1PieceSelected() == SelectedPiece.O) {
+                    game.board.getCellByIndex(GridPane.getRowIndex(cell), GridPane.getColumnIndex(cell)).setStatus(cellStatus.O);
+                }
+                onClickUI(game.checkForSOS(GridPane.getRowIndex(cell), GridPane.getColumnIndex(cell)), cell);
+                game.setPlayerTurn(PlayerTurn.PLAYER2);
+                if (game.player2Type == PlayerType.COMPUTER) {
+                    c = game.computerMove();
+                    if (c.getX() != -1) {
+                        getChildFromGridPaneByRowAndColumn(c.getX(), c.getY()).getChildren().get(0).setVisible(true);
+                        onClickUI(game.checkForSOS(c.getX(), c.getY()), cell);
+                    }
+                    if (game.getPlayerTurn() == PlayerTurn.PLAYER1) {
+                        game.setPlayerTurn(PlayerTurn.PLAYER2);
+                    }
+                    else {
+                        game.setPlayerTurn(PlayerTurn.PLAYER1);
+                    }
+                }
+            } else if (game.getPlayerTurn() == PlayerTurn.PLAYER2) {
+                if (game.getPlayer2PieceSelected() == SelectedPiece.S) {
+                    game.board.getCellByIndex(GridPane.getRowIndex(cell), GridPane.getColumnIndex(cell)).setStatus(cellStatus.S);
+                } else if (game.getPlayer2PieceSelected() == SelectedPiece.O) {
+                    game.board.getCellByIndex(GridPane.getRowIndex(cell), GridPane.getColumnIndex(cell)).setStatus(cellStatus.O);
+                }
+                onClickUI(game.checkForSOS(GridPane.getRowIndex(cell), GridPane.getColumnIndex(cell)), cell);
+                game.setPlayerTurn(PlayerTurn.PLAYER1);
+                if (game.player1Type == PlayerType.COMPUTER) {
+                    c = game.computerMove();
+                    if (c.getX() != -1) {
+                        getChildFromGridPaneByRowAndColumn(c.getX(), c.getY()).getChildren().get(0).setVisible(true);
+                        onClickUI(game.checkForSOS(c.getX(), c.getY()), cell);
+                    }
+                    if (game.getPlayerTurn() == PlayerTurn.PLAYER1) {
+                        game.setPlayerTurn(PlayerTurn.PLAYER2);
+                    }
+                    else {
+                        game.setPlayerTurn(PlayerTurn.PLAYER1);
+                    }
+                }
+            }
+        }
+        if (game.getGameFinished()) {
+            winDisplay();
+        }
+    }
+
     private void winDisplay() {
         ObservableList<Node> childrenList = gameBoard.getChildren();
         for (Node node : childrenList) {
@@ -180,145 +277,8 @@ public class CellController {
     }
 
     private void setClickEventHandler(StackPane cell) {
-
-        ObservableList<Node> childrenList = cell.getChildren();
         cell.setOnMouseClicked(event -> { // Handles mouse click event
-
-            // Processing click in game logic
-            List<Coordinate> SOSList = new ArrayList<>();
-            Coordinate c;
-            if (game.board.getCellByIndex(GridPane.getRowIndex(cell), GridPane.getColumnIndex(cell)).getStatus() == cellStatus.EMPTY) {
-                if (game.getPlayerTurn() == PlayerTurn.PLAYER1) {
-                    if (game.getPlayer1PieceSelected() == SelectedPiece.S) {
-                        game.board.getCellByIndex(GridPane.getRowIndex(cell), GridPane.getColumnIndex(cell)).setStatus(cellStatus.S);
-                    } else if (game.getPlayer1PieceSelected() == SelectedPiece.O) {
-                        game.board.getCellByIndex(GridPane.getRowIndex(cell), GridPane.getColumnIndex(cell)).setStatus(cellStatus.O);
-                    }
-                    SOSList =  game.checkForSOS(GridPane.getRowIndex(cell), GridPane.getColumnIndex(cell));
-                    game.setPlayerTurn(PlayerTurn.PLAYER2);
-                    if (game.player2Type == PlayerType.COMPUTER) {
-                        c = game.computerMove();
-                        if (c.getX() != -1) {
-                            getChildFromGridPaneByRowAndColumn(c.getX(), c.getY()).getChildren().get(0).setVisible(true);
-                            List<Coordinate> computerSOSList = game.checkForSOS(c.getX(), c.getY());
-                            if (!computerSOSList.isEmpty()) {
-                                StackPane startCell = getChildFromGridPaneByRowAndColumn(computerSOSList.get(0).getX(), computerSOSList.get(0).getY());
-                                StackPane endCell = getChildFromGridPaneByRowAndColumn(computerSOSList.get(2).getX(), computerSOSList.get(2).getY());
-                                double startX = startCell.getLayoutX() + (startCell.getWidth() / 2);
-                                double startY = startCell.getLayoutY() + (startCell.getHeight() / 2);
-                                double endX = endCell.getLayoutX() + (startCell.getWidth() / 2);
-                                double endY = endCell.getLayoutY() + (startCell.getHeight() / 2);
-                                Line line = new Line(startX, startY, endX, endY);
-                                line.setStrokeWidth(5);
-                                if (game.getPlayerTurn() == PlayerTurn.PLAYER2) {
-                                    line.setStyle("-fx-stroke: red");
-                                }
-                                else if (game.getPlayerTurn() == PlayerTurn.PLAYER1) {
-                                    line.setStyle("-fx-stroke: blue");
-                                }
-                                gridpaneWrapper.getChildren().add(line);
-                                computerSOSList.remove(0);
-                                computerSOSList.remove(0);
-                                computerSOSList.remove(0);
-                                System.out.println("Line created: " + startX + " " + startY + " " + endX + " " + endY);
-                            }
-                        }
-                        if (game.getPlayerTurn() == PlayerTurn.PLAYER1) {
-                            game.setPlayerTurn(PlayerTurn.PLAYER2);
-                        }
-                        else {
-                            game.setPlayerTurn(PlayerTurn.PLAYER1);
-                        }
-                    }
-                } else if (game.getPlayerTurn() == PlayerTurn.PLAYER2) {
-                    if (game.getPlayer2PieceSelected() == SelectedPiece.S) {
-                        game.board.getCellByIndex(GridPane.getRowIndex(cell), GridPane.getColumnIndex(cell)).setStatus(cellStatus.S);
-                    } else if (game.getPlayer2PieceSelected() == SelectedPiece.O) {
-                        game.board.getCellByIndex(GridPane.getRowIndex(cell), GridPane.getColumnIndex(cell)).setStatus(cellStatus.O);
-                    }
-                    SOSList = game.checkForSOS(GridPane.getRowIndex(cell), GridPane.getColumnIndex(cell));
-                    game.setPlayerTurn(PlayerTurn.PLAYER1);
-                    if (game.player1Type == PlayerType.COMPUTER) {
-                        c = game.computerMove();
-                        if (c.getX() != -1) {
-                            getChildFromGridPaneByRowAndColumn(c.getX(), c.getY()).getChildren().get(0).setVisible(true);
-                            List<Coordinate> computerSOSList = game.checkForSOS(c.getX(), c.getY());
-                            if (!computerSOSList.isEmpty()) {
-                                StackPane startCell = getChildFromGridPaneByRowAndColumn(computerSOSList.get(0).getX(), computerSOSList.get(0).getY());
-                                StackPane endCell = getChildFromGridPaneByRowAndColumn(computerSOSList.get(2).getX(), computerSOSList.get(2).getY());
-                                double startX = startCell.getLayoutX() + (startCell.getWidth() / 2);
-                                double startY = startCell.getLayoutY() + (startCell.getHeight() / 2);
-                                double endX = endCell.getLayoutX() + (startCell.getWidth() / 2);
-                                double endY = endCell.getLayoutY() + (startCell.getHeight() / 2);
-                                Line line = new Line(startX, startY, endX, endY);
-                                line.setStrokeWidth(5);
-                                if (game.getPlayerTurn() == PlayerTurn.PLAYER2) {
-                                    line.setStyle("-fx-stroke: red");
-                                }
-                                else if (game.getPlayerTurn() == PlayerTurn.PLAYER1) {
-                                    line.setStyle("-fx-stroke: blue");
-                                }
-                                gridpaneWrapper.getChildren().add(line);
-                                computerSOSList.remove(0);
-                                computerSOSList.remove(0);
-                                computerSOSList.remove(0);
-                                System.out.println("Line created: " + startX + " " + startY + " " + endX + " " + endY);
-                            }
-                        }
-                        if (game.getPlayerTurn() == PlayerTurn.PLAYER1) {
-                            game.setPlayerTurn(PlayerTurn.PLAYER2);
-                        }
-                        else {
-                            game.setPlayerTurn(PlayerTurn.PLAYER1);
-                        }
-                    }
-                }
-            }
-            if (game.getGameFinished()) {
-                winDisplay();
-            }
-
-            // UI control
-
-            if (SOSList.size() >= 3) {
-                while (SOSList.size() >= 3) {
-                    StackPane startCell = getChildFromGridPaneByRowAndColumn(SOSList.get(0).getX(), SOSList.get(0).getY());
-                    StackPane endCell = getChildFromGridPaneByRowAndColumn(SOSList.get(2).getX(), SOSList.get(2).getY());
-                    double startX = startCell.getLayoutX() + (startCell.getWidth() / 2);
-                    double startY = startCell.getLayoutY() + (startCell.getHeight() / 2);
-                    double endX = endCell.getLayoutX() + (startCell.getWidth() / 2);
-                    double endY = endCell.getLayoutY() + (startCell.getHeight() / 2);
-                    Line line = new Line(startX, startY, endX, endY);
-                    line.setStrokeWidth(5);
-                    if (game.getPlayerTurn() == PlayerTurn.PLAYER2) {
-                        line.setStyle("-fx-stroke: red");
-                    }
-                    else if (game.getPlayerTurn() == PlayerTurn.PLAYER1) {
-                        line.setStyle("-fx-stroke: blue");
-                    }
-                    gridpaneWrapper.getChildren().add(line);
-                    SOSList.remove(0);
-                    SOSList.remove(0);
-                    SOSList.remove(0);
-                    System.out.println("Line created: " + startX + " " + startY + " " + endX + " " + endY);
-                }
-            }
-
-            switch (game.board.getCellByIndex(GridPane.getRowIndex(cell), GridPane.getColumnIndex(cell)).getStatus()) {
-                case S:
-                    childrenList.get(0).setVisible(true);
-                    break;
-                case O:
-                    childrenList.get(1).setVisible(true);
-                    break;
-            }
-            if (game.getPlayerTurn() == PlayerTurn.PLAYER1) {
-                player2Pane.setStyle("-fx-background-color: #FFFFFF");
-                player1Pane.setStyle("-fx-background-color: #6D9DD5");
-            } else if (game.getPlayerTurn() == PlayerTurn.PLAYER2) {
-                player2Pane.setStyle("-fx-background-color: #CC4B23");
-                player1Pane.setStyle("-fx-background-color: #FFFFFF");
-            }
+            onClickData(cell);
         });
 
     }
